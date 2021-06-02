790609529f7643da709cb90f7cddc3101c1df5c8
awongCM <awong.cm@gmail.com>
2021-06-02 15:59:26 +1000

    implemented the function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,50 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+const STATUS_CODE_KEYS = {
+  [200]: "Arnie Quote",
+  [500]: "FAILURE",
+};
+
+// handling exceptions
+const ERROR_MESSAGE = "Malformed JSON Response";
+const ERROR_KEY = "Unhandled Status Code";
+
+const retrieveStatusKey = (status) => {
+  return STATUS_CODE_KEYS[status] === undefined
+    ? ERROR_KEY
+    : STATUS_CODE_KEYS[status];
+};
+
+const makeURLPromise = async (url) => {
+  let response = await httpGet(url);
+  const { status, body } = response;
+
+  const message = parseAndRetrieveJSONBody(body);
+  const statusKey = retrieveStatusKey(status);
+
+  return {
+    [statusKey]: message,
+  };
+};
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  let urlPromises = urls.map(makeURLPromise);
+
+  return Promise.all(urlPromises);
+};
+
+const parseAndRetrieveJSONBody = (body) => {
+  try {
+    let response = JSON.parse(body);
+    if ("message" in response) {
+      return response["message"];
+    } else {
+      // gives back whatever the server response comes back with
+      return JSON.stringify(response);
+    }
+  } catch (error) {
+    return ERROR_MESSAGE;
+  }
 };
 
 module.exports = {
